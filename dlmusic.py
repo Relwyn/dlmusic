6fc4d4f95880223c46adfee6d46cc7b62cdec6a9
Relwyn <maerten.nicolas62@gmail.com>
2019-03-12 07:18:59 +0100

    MV now make by Python

--- dlmusic.py
+++ dlmusic.py
@@ -1,7 +1,7 @@
 import youtube_dl
 import os
 import subprocess
-
+import shutil
 ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
@@ -13,7 +13,7 @@ ydl_opts = {
 print("Debut du programme \nRecuperation des liens youtube")
 with open("liens.txt") as f:
     content = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
+
 content = [x.strip() for x in content]
 
 print("Telechargement des musiques :")
@@ -24,20 +24,18 @@ for url in content:
 print("Deplacement des Musiques pour modifier leurs noms")
 subprocess.call("mv ./*.mp3 ./Musique", shell=True)
 directory = "./Musique"
+
+print("Suppression de l id present dans le nom du fichier")
 for filename in os.listdir('./Musique'):
     print(filename)
     path = os.path.join(directory, filename)
     rep = filename.replace(filename[-16:], ".mp3")
     target = os.path.join(directory, rep)
     os.rename(path, target)
-
-print("Placement des musiques dans le Dossier Musique du Desktop")
-subprocess.call("mv ./Musique/*.mp3 ../Musique", shell=True)
+    shutil.move(target,"../../Musique")
 
 print("Remise au propre du fichier liens.txt")
 subprocess.call("echo "">liens.txt", shell=True)
 
 print("Complete")
 
-
-"""/Library/Frameworks/Python.framework/Versions/3.6/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin   CHEMIN VERS PYTHON"""
